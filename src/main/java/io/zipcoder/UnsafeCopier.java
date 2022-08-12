46da9b9903719d0f300976da73365d2ef6c74349
tristan <jametri014@gmail.com>
2022-08-11 20:40:58 -0400

    Moddifed unsafeCopier class

--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,14 +1,17 @@
 package io.zipcoder;
 
+import java.util.Random;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
 public class UnsafeCopier extends Copier {
-
+    Random random = new Random();
     public UnsafeCopier(String toCopy) {
         super(toCopy);
     }
 
     public void run() {
+        super.run();
     }
 }
